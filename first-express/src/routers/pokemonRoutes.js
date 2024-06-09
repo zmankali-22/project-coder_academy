27f339986038fcb44d9f2ba8a5d445161a23ca7a
utsav <sav.acharya22@gmail.com>
2024-06-09 13:14:13 +1000

    new pokemon routes

--- first-express/src/routers/pokemonRoutes.js
+++ first-express/src/routers/pokemonRoutes.js
@@ -1,15 +1,24 @@
-
-const express = require('express')
-const router = express.Router()
-
+const express = require("express");
+const router = express.Router();
 
 router.get("/", (request, response) => {
-    response.json({message: "Router route activated"})
-} )
+  response.json({ message: "Router route activated" });
+});
+
+router.get("/:pokemonNumber", (request, response) => {
+  let retrivedNumberFromUrl = request.params.pokemonNumber;
+
+  response.json({
+    number: retrivedNumberFromUrl,
+  });
+});
 
 router.get("/random", (request, response) => {
-    response.json({message: "Random route activated"})
-})
+  response.json({ message: "Random route activated" });
+});
 
+router.post("/", (request, response) => {
+  response.json({ message: "Nah nah no posting here" });
+});
 
-module.exports = router
+module.exports = router;
